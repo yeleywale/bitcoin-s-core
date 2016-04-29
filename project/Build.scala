9b5e2a707cf59c9ade2479f3b5493bb441eb9722
Thomas McCabe <TomMcCabe@users.noreply.github.com>
2016-04-29 10:39:25 -0500

    Fix logging

--- project/Build.scala
+++ project/Build.scala
@@ -2,16 +2,18 @@ import sbt._
 import Keys._ 
 object BitcoinSBuild extends Build {
 
-  val appName = "bitcoin-s"
+  val appName = "bitcoins"
   val appV = "0.0.1" 
-  val scalaV = "2.11.4"
+  val scalaV = "2.11.7"
   val organization = "org.bitcoins"
   val slf4jV = "1.7.5"
+  val logbackV = "1.0.13"
   val appDependencies = Seq(
     "org.scalatest" % "scalatest_2.11" % "2.2.0",
     ("org.bitcoinj" % "bitcoinj-core" % "0.13.3").exclude("org.slf4j", "slf4j-api"),
     "org.slf4j" % "slf4j-api" % slf4jV /*% "provided"*/,
-    "io.spray" %%  "spray-json" % "1.3.0" withSources() withJavadoc()
+    "io.spray" %%  "spray-json" % "1.3.0" withSources() withJavadoc(),
+     "ch.qos.logback" % "logback-classic" % logbackV
   )
   
   val main = Project(appName, file(".")).enablePlugins().settings(
@@ -22,4 +24,3 @@ object BitcoinSBuild extends Build {
     scalacOptions ++= Seq("-unchecked", "-deprecation")  
   )
 } 
-
